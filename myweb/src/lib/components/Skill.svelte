330dd305ece7f179294db93abf83742d3553c98d
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2025-04-10 20:28:23 +0530

    animated skills

--- myweb/src/lib/components/Skill.svelte
+++ myweb/src/lib/components/Skill.svelte
@@ -2,9 +2,9 @@
     const {skill} = $props();
 </script>
 
-<div class="group group-hover:scale-110 transition-transform duration-500 ease-in-out transform hover:shadow-lg dark:hover:shadow-green-400 hover:shadow-green-800 rounded-lg dark:bg-gray-800 p-4 m-2 flex flex-col justify-center items-center text-center bg-green-700">
+<div class="group hover:scale-110 transition-all duration-500 ease-in-out transform hover:shadow-lg dark:hover:shadow-green-400 hover:shadow-green-950 rounded-lg dark:bg-gray-800 p-4 m-2 flex flex-col justify-center items-center text-center bg-green-700 ">
     <img src={skill.icon} alt={skill.name} class="w-16 h-16 m-2 group-hover:scale-110 transition-transform duration-500 ease-in-out transform" />
-    <div class="flex flex-col justify-center items-center text-center duration-500 ease-in-out group-hover:scale-120 transform scale-100">
-        <h4 class="dark:text-white text-green-900 font-bold text-xl">{skill.name}</h4>
+    <div class="flex flex-col justify-center items-center text-center duration-500 ease-in-out group-hover:scale-120 transform scale-100 group-hover:-translate-y-1">
+        <h4 class="text-white font-bold text-xl">{skill.name}</h4>
     </div>
-</div> 
+</div>
--- myweb/src/lib/components/SkillBoard.svelte
+++ myweb/src/lib/components/SkillBoard.svelte
@@ -3,11 +3,11 @@
     const { skillCategory } = $props();
 </script>
 
-<div class="border-2 dark:border-green-400 border-green-900 flex flex-wrap rounded-lg m-5 p-5 w-3/4 flex-col">
-    <h4 class="dark:text-white text-green-900 font-extrabold text-4xl m-5 text-center">{skillCategory.category}</h4>
+<div class="border-2 dark:border-green-400 border-green-900 flex flex-wrap rounded-lg m-5 p-5 md:w-11/12 flex-col">
+    <h4 class="dark:text-white text-green-900 font-extrabold md:text-4xl text-xl m-5 text-center">{skillCategory.category}</h4>
     <hr class="m-5 h-1 border-0 dark:bg-green-900 bg-black">
     <div class="flex flex-wrap mx-2 justify-center">
-        {#each skillCategory.items as skill}
+        {#each skillCategory.items as skill, index}
             <Skill {skill} />
         {/each}
     </div>
